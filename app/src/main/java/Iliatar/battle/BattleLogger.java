90b40c5335e6312d58bca62b4e99fb5eacb8886a
Iliatar <a.u.tarasov@gmail.com>
2024-05-31 09:30:48 +0300

    log4j2 configurized

--- app/src/main/java/Iliatar/battle/BattleLogger.java
+++ app/src/main/java/Iliatar/battle/BattleLogger.java
@@ -1,13 +1,17 @@
 package Iliatar.battle;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 //TODO убрать статику
 public class BattleLogger {
     static BattleManager battleManager;
+    private static final Logger logger = LogManager.getLogger(BattleLogger.class);
 
     public static void initiate(BattleManager battleManager) {
         BattleLogger.battleManager = battleManager;
     }
     public static void logMessage(String message) {
-        System.out.println(battleManager.getTurnNumber() + ": " + message);
+        logger.info(battleManager.getTurnNumber() + ":  " + message);
     }
 }
